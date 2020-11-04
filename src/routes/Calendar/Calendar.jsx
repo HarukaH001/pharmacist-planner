269d8eee5c8f6e2e6bd805747995b98595ec218e
Natasit Saragan <haruka.hime001@gmail.com>
2020-11-05 03:51:26 +0700

    test month pickers and failed

--- src/routes/Calendar/Calendar.jsx
+++ src/routes/Calendar/Calendar.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './Calendar.scss';
 // import { Link, useLocation, useHistory } from 'react-router-dom';
 
 export const Calendar = () => {
-    // const history = useHistory()
+    const [selectedMonth, setSelectedMonth] = useState({year: 2016, month: 7})
+    const picker = useRef()
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
     return (
         <div className="Calendar">
-            
+
         </div>
     )
 }
