515ed3629d420674926e15d22bc4626d19c2e367
Natasit Saragan <haruka.hime001@gmail.com>
2020-11-22 00:05:08 +0700

    timestamp

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
     return (
         <div className="Home">
             <div className="container">
-                Home
+                Home 23.00 - 00.00
             </div>
         </div>
     )
