28364fcfd8636ebf71ec51d1dfa3c01fe164bd77
Natasit Saragan <haruka.hime001@gmail.com>
2020-12-15 15:02:21 +0700

    Home resize : smallest 1366x625

--- src/App.jsx
+++ src/App.jsx
@@ -53,21 +53,19 @@ const App = () => {
                 Home
               </Button>
             </Link>
+
             <div style={{ height: '10px' }}></div>
-            <Link to="/calendar" onClick={() => setPath('calendar')} style={{ textDecoration: 'none' }}>
-              <Button variant={path === 'calendar' ? "primary" : "light"} size="sm" block className="nofocus" style={path === 'calendar' ? {} : { backgroundColor: 'white', border: 'none' }}>
-                {path === 'calendar' ? <svg width="1rem" height="1rem" viewBox="0 0 16 16" className="bi bi-calendar2-date" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                  <path fillRule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
-                  <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4zm3.945 8.688V7.354h-.633A12.6 12.6 0 0 0 4.5 8.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z" />
-                </svg> : <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-calendar2-date-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 3.5c0-.276.244-.5.545-.5h10.91c.3 0 .545.224.545.5v1c0 .276-.244.5-.546.5H2.545C2.245 5 2 4.776 2 4.5v-1zm7.336 9.29c-1.11 0-1.656-.767-1.703-1.407h.683c.043.37.387.82 1.051.82.844 0 1.301-.848 1.305-2.164h-.027c-.153.414-.637.79-1.383.79-.852 0-1.676-.61-1.676-1.77 0-1.137.871-1.809 1.797-1.809 1.172 0 1.953.734 1.953 2.668 0 1.805-.742 2.871-2 2.871zm.066-2.544c.625 0 1.184-.484 1.184-1.18 0-.832-.527-1.23-1.16-1.23-.586 0-1.168.387-1.168 1.21 0 .817.543 1.2 1.144 1.2zm-2.957-2.89v5.332H5.77v-4.61h-.012c-.29.156-.883.52-1.258.777V8.16a12.6 12.6 0 0 1 1.313-.805h.632z" />
-                  </svg>}
-                Calendar
+            <div style={{height:"43px"}}>
+              <Button variant={"light"} size="sm" block className="admin nofocus" style={{ backgroundColor: 'white', border: 'none' }}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-person-badge-fill" viewBox="0 0 16 16">
+                  <path fillRule="evenodd" d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z"/>
+                </svg>
+                Staff Manage
               </Button>
-            </Link>
+            </div>
             <div style={{ height: '10px' }}></div>
             <Link to="/staff" onClick={() => setPath('staff')} style={{ textDecoration: 'none' }}>
-              <Button variant={path === 'staff' ? "primary" : "light"} size="sm" block className="nofocus" style={path === 'staff' ? {} : { backgroundColor: 'white', border: 'none' }}>
+              <Button variant={path === 'staff' ? "primary" : "light"} size="sm" block className="sp nofocus" style={path === 'staff' ? {} : { backgroundColor: 'white', border: 'none' }}>
                 {path !== 'staff' ? <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-people-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style={{ marginLeft: '7px' }}>
                   <path fillRule="evenodd" d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
                 </svg> : <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-people" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -78,7 +76,7 @@ const App = () => {
             </Link>
             <div style={{ height: '10px' }}></div>
             <Link to="/pharmacist" onClick={() => setPath('pharmacist')} style={{ textDecoration: 'none' }}>
-              <Button variant={path === 'pharmacist' ? "primary" : "light"} size="sm" block className="nofocus" style={path === 'pharmacist' ? {} : { backgroundColor: 'white', border: 'none' }}>
+              <Button variant={path === 'pharmacist' ? "primary" : "light"} size="sm" block className="sp nofocus" style={path === 'pharmacist' ? {} : { backgroundColor: 'white', border: 'none' }}>
                 {path !== 'pharmacist' ? <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-pie-chart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                   <path d="M15.985 8.5H8.207l-5.5 5.5a8 8 0 0 0 13.277-5.5zM2 13.292A8 8 0 0 1 7.5.015v7.778l-5.5 5.5zM8.5.015V7.5h7.485A8.001 8.001 0 0 0 8.5.015z" />
                 </svg> : <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-pie-chart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -86,6 +84,19 @@ const App = () => {
                     <path fillRule="evenodd" d="M7.5 7.793V1h1v6.5H15v1H8.207l-4.853 4.854-.708-.708L7.5 7.793z" />
                   </svg>}
                 Pharmacist
+              </Button>
+            </Link>
+
+            <div style={{ height: '10px' }}></div>
+            <Link to="/calendar" onClick={() => setPath('calendar')} style={{ textDecoration: 'none' }}>
+              <Button variant={path === 'calendar' ? "primary" : "light"} size="sm" block className="nofocus" style={path === 'calendar' ? {} : { backgroundColor: 'white', border: 'none' }}>
+                {path === 'calendar' ? <svg width="1rem" height="1rem" viewBox="0 0 16 16" className="bi bi-calendar2-date" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
+                  <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4zm3.945 8.688V7.354h-.633A12.6 12.6 0 0 0 4.5 8.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z" />
+                </svg> : <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-calendar2-date-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 3.5c0-.276.244-.5.545-.5h10.91c.3 0 .545.224.545.5v1c0 .276-.244.5-.546.5H2.545C2.245 5 2 4.776 2 4.5v-1zm7.336 9.29c-1.11 0-1.656-.767-1.703-1.407h.683c.043.37.387.82 1.051.82.844 0 1.301-.848 1.305-2.164h-.027c-.153.414-.637.79-1.383.79-.852 0-1.676-.61-1.676-1.77 0-1.137.871-1.809 1.797-1.809 1.172 0 1.953.734 1.953 2.668 0 1.805-.742 2.871-2 2.871zm.066-2.544c.625 0 1.184-.484 1.184-1.18 0-.832-.527-1.23-1.16-1.23-.586 0-1.168.387-1.168 1.21 0 .817.543 1.2 1.144 1.2zm-2.957-2.89v5.332H5.77v-4.61h-.012c-.29.156-.883.52-1.258.777V8.16a12.6 12.6 0 0 1 1.313-.805h.632z" />
+                  </svg>}
+                Calendar
               </Button>
             </Link>
           </div>
